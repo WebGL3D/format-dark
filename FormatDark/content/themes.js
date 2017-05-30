eebe342a8a59710ea3fabfc0c4558f23f29bac54
WebGL3D <OgreLandBob@gmail.com>
2017-05-29 22:27:10 -0700

    First cut

---
+++ FormatDark/content/themes.js
@@ -0,0 +1,39 @@
+﻿/*
+	content/themes.js [05/29/2017]
+*/
+$(function () {
+	var highlight = false;
+
+	if (document.contentType.startsWith("application/json") || location.href.endsWith(".json")) {
+		highlight = true;
+
+		var tag = $("pre").attr("data-extension-take-over", ext.id);
+		var json = JSON.parse(tag.html());
+		var original = JSON.stringify(json);
+		var pretty = JSON.stringify(json, null, "\t");
+
+		var isPretty = false;
+		$(document).keyup(function (e) {
+			if (e.keyCode == 13) {
+				isPretty = !isPretty;
+				$("pre").text(isPretty ? pretty : original);
+				hljs.highlightBlock(document.querySelector("html"));
+			}
+		});
+	} else if (document.contentType.startsWith("application/javascript") || location.href.endsWith(".js")) {
+		highlight = true;
+
+		// TODO: Pretty-print?
+	} else if (document.contentType.startsWith("text/css") || location.href.endsWith(".css")) {
+		highlight = true;
+
+		// TODO: Pretty-print?
+	}
+
+	if (highlight) {
+		hljs.highlightBlock(document.querySelector("html"));
+	}
+});
+
+
+// WebGL3D
