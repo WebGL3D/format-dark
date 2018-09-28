4b3a2647321cc984c42b3350e0e5f6ee5b69a5e3
WebGL3D <OgreLandBob@gmail.com>
2018-09-27 21:25:47 -0700

    Beautify css

--- FormatDark/content/themes.js
+++ FormatDark/content/themes.js
@@ -2,42 +2,60 @@
 	content/themes.js [05/29/2017]
 */
 $(function () {
+	var languageName = "";
 	var highlight = false;
+	var isPretty = false;
+	var original, pretty;
 
-	if (document.contentType.startsWith("text/html")) {
+	var doHighlight = function () {
+		if (languageName) {
+			document.documentElement.classList.add(languageName);
+		}
+
+		document.head.innerHTML = "";
+		hljs.highlightBlock(document.documentElement);
+	};
+
+	if (document.contentType.includes("html")) {
 		return;
 	}
 
 	if (document.contentType.startsWith("application/json") || location.href.endsWith(".json")) {
 		highlight = true;
+		languageName = "json";
 
 		var tag = $("pre").attr("data-extension-take-over", ext.id);
 		var json = JSON.parse(tag.html());
-		var original = JSON.stringify(json);
-		var pretty = JSON.stringify(json, null, "\t");
+		original = JSON.stringify(json);
+		pretty = JSON.stringify(json, null, "\t");
+	} else if (document.contentType.includes("javascript") || location.href.endsWith(".js")) {
+		highlight = true;
+		languageName = "javascript";
 
-		var isPretty = false;
-		$(document).keyup(function (e) {
-			if (e.keyCode == 13) {
-				isPretty = !isPretty;
-				$("pre").text(isPretty ? pretty : original);
-				document.head.innerHTML = "";
-				hljs.highlightBlock(document.documentElement);
-			}
+		// TODO: Pretty-print?
+	} else if (document.contentType.includes("css") || location.href.endsWith(".css")) {
+		highlight = true;
+		languageName = "css";
+
+		original = document.documentElement.innerText;
+		pretty = cssbeautify(original, {
+			indent: "\t",
+			autosemicolon: true,
+			openbrace: "end-of-line"
 		});
-	} else if (document.contentType.startsWith("application/javascript") || location.href.endsWith(".js")) {
-		highlight = true;
-
-		// TODO: Pretty-print?
-	} else if (document.contentType.startsWith("text/css") || location.href.endsWith(".css")) {
-		highlight = true;
-
-		// TODO: Pretty-print?
 	}
 
+	$(document).keyup(function (e) {
+		if (e.keyCode === 13 && pretty && highlight) {
+			isPretty = !isPretty;
+			var pre = $("pre");
+			pre.text(isPretty ? pretty : original);
+			doHighlight();
+		}
+	});
+
 	if (highlight) {
-		document.head.innerHTML = "";
-		hljs.highlightBlock(document.documentElement);
+		doHighlight();
 	}
 });
 
